79b0297fc1d0fb8687ea7e50c98a2cf30644d09f
mguppy <mguppy34@gmail.com>
2021-10-07 10:04:06 -0500

    sum array function working correctly

--- 04-03-sum-array/Unsolved/sum-array.js
+++ 04-03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,18 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+
+var sumArray = function (arr) {
+
+    //Declare sum variable
+    var result = 0;
+
+    //Loop through each number in the array and add to sum variable
+    for (let i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        result = result + arr[i];
+    }
+
+    //Return sum
+    console.log(result);
+    return result;
+};
