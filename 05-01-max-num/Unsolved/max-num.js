730afef1bc834bcbbc89b2620fceebefeb685b1c
mguppy <mguppy34@gmail.com>
2021-10-07 14:50:20 -0500

    max num function working correctly

--- 05-01-max-num/Unsolved/max-num.js
+++ 05-01-max-num/Unsolved/max-num.js
@@ -1,3 +1,14 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    var result = arr[0];
+    // console.log(result);
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > result) {
+            result = arr[i];
+            // console.log(result);
+        }
+    }
+    return result;
+};
