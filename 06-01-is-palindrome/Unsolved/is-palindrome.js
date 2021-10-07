1cd5e44b17c68cf8094be1be425b115a3f163d78
mguppy <mguppy34@gmail.com>
2021-10-07 15:59:58 -0500

    ispalindrome function working correctly

--- 06-01-is-palindrome/Unsolved/is-palindrome.js
+++ 06-01-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,22 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    // Separate string by characters
+    // var characters = str.split("");
+    // console.log(characters);
+
+    // For loop to go through the characters to determine if the first half of the characters match the last half
+    for (let i = 0; i < str.length; i++) {
+        // Convert all characters in string to lowercase so there are no issues in matching
+        str = str.toLowerCase();
+        // If character 0 does not match last character in string, stop loop
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+
+    // If string is palindrome, return true
+
+    // If string is not, return false
+};
