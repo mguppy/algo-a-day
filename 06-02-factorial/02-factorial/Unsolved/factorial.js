5ff952be9a0e413e8f995d2a520437a52f71b02d
mguppy <mguppy34@gmail.com>
2021-10-26 14:15:31 -0500

    factorial algo solved

--- 06-02-factorial/02-factorial/Unsolved/factorial.js
+++ 06-02-factorial/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,18 @@
 // Write code to create a function that returns the factorial of `num`
 
-var factorial = function(num) {};
+var factorial = function(num) {
+
+    // 4 factorial would equal 24; 4*3*2*1 = 24
+
+    // Should return 1 if num is 0
+    if (num === 0) {
+        return 1;
+    }
+
+    else {
+    for (var i = num-1; i >= 1; i--) {
+        num = i * num;
+    }
+    return num;
+}
+};
